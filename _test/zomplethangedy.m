3264432328998b62cd3becdc9b6b61c6a9939715
Tim Brandle <tbrandle53@gmail.com>
2019-03-15 10:03:37 -0600

    Renamed item

---
+++ _test/zomplethangedy.m
@@ -0,0 +1,225 @@
+---
+testName: JaMes
+testjobTitle: test
+testbio: null
+testemail: null
+testtwitter: null
+testgithub: null
+testlinkedin: null
+img_path: /upload/image/png/1552346345917_img_path_482bc49b6f9fecd8aa4443ca379bbd69.png
+categories: thing1 thing2 thing3
+recommendations:
+  - title: >-
+      <h4 class='no-margin'>Recommendation #1:</h4> Encourage orderly growth and
+      development.
+    content: >-
+      Orderly growth and development requires a balance of residential,
+      commercial, and employment-oriented uses.  This encourages a sensible
+      mixture (rather than segregation) of uses to help create vibrant areas
+      throughout the city that offer a range of jobs and daily services and
+      amenities in close proximity to where people live.  It also requires
+      aggregation of activity to create centers, as well as compatible
+      transitions between neighboring properties.  In this sense, orderly growth
+      and development seeks to preserve the functional and desirable character
+      of assets present today, while encouraging new development which is also
+      functional and desirable.
+    image: null
+    image-2: null
+  - title: >-
+      <h4 class='no-margin'>Recommendation #2:</h4> Align the Zoning Code with
+      the Future Land Use Plan.
+    content: >-
+      The primary function of the Future Land Use Plan is to guide and inform
+      zoning practices in the City.  The Future Land Use Plan ensures that
+      zoning decisions that are made by the City align with the community’s
+      vision, as expressed in the master plan of the City (SA Tomorrow and the
+      Brooks Area Regional Center Plan).  Once a future land use plan is
+      adopted, the zoning code must align with the future land use plan It is
+      not, however, a direct translation, as there are multiple zoning districts
+      that are applicable within each land use category.  The land use
+      categories allow for a range of zoning districts which are generally
+      complementary, to avoid conflicts between adjacent uses, to preserve
+      property values, and to give clearer definition to the spaces within the
+      community. The City Planning Department will oversee alignment of the
+      Zoning Code with the Future Land Use Plan.
+    image: null
+    image-2: null
+  - title: >-
+      <h4 class='no-margin'>Recommendation #3:</h4> Promote Trail-oriented
+      development and connectivity to natural assets.
+    content: >-
+      The Brooks Area Regional Center is adjacent to Mission Reach, and to the
+      San Antonio River.  These two cultural and natural assets should to be
+      accessible to the community of the Brooks Area Regional Center.  Salado
+      Creek runs parallel to the San Antonio River on the east side of the
+      Brooks Area and should be recognized as a natural asset that should be
+      connected and used. Additionally, this area is characterized by beautiful
+      topography and riparian areas, which should be used not only for
+      recreation, but as the site of park-and trail-oriented development as
+      envisioned in the <a href='https://sacompplan.com/place-types/'
+      target='_blank'>SA Tomorrow Comprehensive Plan</a>. Land uses on adjacent
+      properties should capture the potential provided by these natural assets,
+      while minimizing the impact that development can have on environmentally
+      sensitive areas.
+    image: null
+    image-2: null
+  - title: >-
+      <h4 class='no-margin'>Recommendation #4:</h4> Respect and respond to
+      topography and geographic constraints when planning new development and
+      public improvements.
+    content: >-
+      The Brooks Area includes significant topography and other geographic
+      features that have historically not been adequately addressed by
+      development. When development regulations do not adequately account for
+      storm water runoff, the rains that are characteristic of the San Antonio
+      area can have a detrimental impact on neighboring properties. 
+      Furthermore, topographic and geographic features, such as floodplains and
+      steep slopes, can make development more complicated and costly. Measures
+      that respond to these natural constraints and actively minimize the
+      negative impact of development on natural systems are needed in the Brooks
+      Area Regional Center.
+    image: null
+    image-2: null
+  - title: >-
+      <h4 class='no-margin'>Recommendation #5:</h4> Encourage business
+      retention, commercial property stability and adaptive re-use where
+      appropriate.
+    content: >-
+      Many of the commercial areas in the Brooks Area Regional Center are
+      aging.  This makes them vulnerable to decline and displacement if land use
+      measures are not in place to encourage business retention, property
+      maintenance, renovation and adaptive re-use.  Maintaining existing
+      businesses and commercial areas promotes stability in property values for
+      the area and ensures adequate commercial and employment accessibility for
+      the surrounding neighborhoods. Encouraging and supporting higher-intensity
+      development and a mix of uses through Mixed-Use land use designations can
+      support these goals. Measures that accommodate revitalization efforts and
+      business vitality should be employed in commercial areas where
+      preservation of existing businesses of value to the community.
+    image: sdf
+    image-2: null
+
+---
+LS0tCnRlc3ROYW1lOiBKYU1lcwp0ZXN0am9iVGl0bGU6IHRlc3QKdGVzdGJp
+bzogbnVsbAp0ZXN0ZW1haWw6IG51bGwKdGVzdHR3aXR0ZXI6IG51bGwKdGVz
+dGdpdGh1YjogbnVsbAp0ZXN0bGlua2VkaW46IG51bGwKaW1nX3BhdGg6IC91
+cGxvYWQvaW1hZ2UvcG5nLzE1NTIzNDYzNDU5MTdfaW1nX3BhdGhfNDgyYmM0
+OWI2ZjlmZWNkOGFhNDQ0M2NhMzc5YmJkNjkucG5nCmNhdGVnb3JpZXM6IHRo
+aW5nMSB0aGluZzIgdGhpbmczCnJlY29tbWVuZGF0aW9uczoKICAtIHRpdGxl
+OiA+LQogICAgICA8aDQgY2xhc3M9J25vLW1hcmdpbic+UmVjb21tZW5kYXRp
+b24gIzE6PC9oND4gRW5jb3VyYWdlIG9yZGVybHkgZ3Jvd3RoIGFuZAogICAg
+ICBkZXZlbG9wbWVudC4KICAgIGNvbnRlbnQ6ID4tCiAgICAgIE9yZGVybHkg
+Z3Jvd3RoIGFuZCBkZXZlbG9wbWVudCByZXF1aXJlcyBhIGJhbGFuY2Ugb2Yg
+cmVzaWRlbnRpYWwsCiAgICAgIGNvbW1lcmNpYWwsIGFuZCBlbXBsb3ltZW50
+LW9yaWVudGVkIHVzZXMuICBUaGlzIGVuY291cmFnZXMgYSBzZW5zaWJsZQog
+ICAgICBtaXh0dXJlIChyYXRoZXIgdGhhbiBzZWdyZWdhdGlvbikgb2YgdXNl
+cyB0byBoZWxwIGNyZWF0ZSB2aWJyYW50IGFyZWFzCiAgICAgIHRocm91Z2hv
+dXQgdGhlIGNpdHkgdGhhdCBvZmZlciBhIHJhbmdlIG9mIGpvYnMgYW5kIGRh
+aWx5IHNlcnZpY2VzIGFuZAogICAgICBhbWVuaXRpZXMgaW4gY2xvc2UgcHJv
+eGltaXR5IHRvIHdoZXJlIHBlb3BsZSBsaXZlLiAgSXQgYWxzbyByZXF1aXJl
+cwogICAgICBhZ2dyZWdhdGlvbiBvZiBhY3Rpdml0eSB0byBjcmVhdGUgY2Vu
+dGVycywgYXMgd2VsbCBhcyBjb21wYXRpYmxlCiAgICAgIHRyYW5zaXRpb25z
+IGJldHdlZW4gbmVpZ2hib3JpbmcgcHJvcGVydGllcy4gIEluIHRoaXMgc2Vu
+c2UsIG9yZGVybHkgZ3Jvd3RoCiAgICAgIGFuZCBkZXZlbG9wbWVudCBzZWVr
+cyB0byBwcmVzZXJ2ZSB0aGUgZnVuY3Rpb25hbCBhbmQgZGVzaXJhYmxlIGNo
+YXJhY3RlcgogICAgICBvZiBhc3NldHMgcHJlc2VudCB0b2RheSwgd2hpbGUg
+ZW5jb3VyYWdpbmcgbmV3IGRldmVsb3BtZW50IHdoaWNoIGlzIGFsc28KICAg
+ICAgZnVuY3Rpb25hbCBhbmQgZGVzaXJhYmxlLgogICAgaW1hZ2U6IG51bGwK
+ICAgIGltYWdlLTI6IG51bGwKICAtIHRpdGxlOiA+LQogICAgICA8aDQgY2xh
+c3M9J25vLW1hcmdpbic+UmVjb21tZW5kYXRpb24gIzI6PC9oND4gQWxpZ24g
+dGhlIFpvbmluZyBDb2RlIHdpdGgKICAgICAgdGhlIEZ1dHVyZSBMYW5kIFVz
+ZSBQbGFuLgogICAgY29udGVudDogPi0KICAgICAgVGhlIHByaW1hcnkgZnVu
+Y3Rpb24gb2YgdGhlIEZ1dHVyZSBMYW5kIFVzZSBQbGFuIGlzIHRvIGd1aWRl
+IGFuZCBpbmZvcm0KICAgICAgem9uaW5nIHByYWN0aWNlcyBpbiB0aGUgQ2l0
+eS4gIFRoZSBGdXR1cmUgTGFuZCBVc2UgUGxhbiBlbnN1cmVzIHRoYXQKICAg
+ICAgem9uaW5nIGRlY2lzaW9ucyB0aGF0IGFyZSBtYWRlIGJ5IHRoZSBDaXR5
+IGFsaWduIHdpdGggdGhlIGNvbW11bml0eeKAmXMKICAgICAgdmlzaW9uLCBh
+cyBleHByZXNzZWQgaW4gdGhlIG1hc3RlciBwbGFuIG9mIHRoZSBDaXR5IChT
+QSBUb21vcnJvdyBhbmQgdGhlCiAgICAgIEJyb29rcyBBcmVhIFJlZ2lvbmFs
+IENlbnRlciBQbGFuKS4gIE9uY2UgYSBmdXR1cmUgbGFuZCB1c2UgcGxhbiBp
+cwogICAgICBhZG9wdGVkLCB0aGUgem9uaW5nIGNvZGUgbXVzdCBhbGlnbiB3
+aXRoIHRoZSBmdXR1cmUgbGFuZCB1c2UgcGxhbiBJdCBpcwogICAgICBub3Qs
+IGhvd2V2ZXIsIGEgZGlyZWN0IHRyYW5zbGF0aW9uLCBhcyB0aGVyZSBhcmUg
+bXVsdGlwbGUgem9uaW5nIGRpc3RyaWN0cwogICAgICB0aGF0IGFyZSBhcHBs
+aWNhYmxlIHdpdGhpbiBlYWNoIGxhbmQgdXNlIGNhdGVnb3J5LiAgVGhlIGxh
+bmQgdXNlCiAgICAgIGNhdGVnb3JpZXMgYWxsb3cgZm9yIGEgcmFuZ2Ugb2Yg
+em9uaW5nIGRpc3RyaWN0cyB3aGljaCBhcmUgZ2VuZXJhbGx5CiAgICAgIGNv
+bXBsZW1lbnRhcnksIHRvIGF2b2lkIGNvbmZsaWN0cyBiZXR3ZWVuIGFkamFj
+ZW50IHVzZXMsIHRvIHByZXNlcnZlCiAgICAgIHByb3BlcnR5IHZhbHVlcywg
+YW5kIHRvIGdpdmUgY2xlYXJlciBkZWZpbml0aW9uIHRvIHRoZSBzcGFjZXMg
+d2l0aGluIHRoZQogICAgICBjb21tdW5pdHkuIFRoZSBDaXR5IFBsYW5uaW5n
+IERlcGFydG1lbnQgd2lsbCBvdmVyc2VlIGFsaWdubWVudCBvZiB0aGUKICAg
+ICAgWm9uaW5nIENvZGUgd2l0aCB0aGUgRnV0dXJlIExhbmQgVXNlIFBsYW4u
+CiAgICBpbWFnZTogbnVsbAogICAgaW1hZ2UtMjogbnVsbAogIC0gdGl0bGU6
+ID4tCiAgICAgIDxoNCBjbGFzcz0nbm8tbWFyZ2luJz5SZWNvbW1lbmRhdGlv
+biAjMzo8L2g0PiBQcm9tb3RlIFRyYWlsLW9yaWVudGVkCiAgICAgIGRldmVs
+b3BtZW50IGFuZCBjb25uZWN0aXZpdHkgdG8gbmF0dXJhbCBhc3NldHMuCiAg
+ICBjb250ZW50OiA+LQogICAgICBUaGUgQnJvb2tzIEFyZWEgUmVnaW9uYWwg
+Q2VudGVyIGlzIGFkamFjZW50IHRvIE1pc3Npb24gUmVhY2gsIGFuZCB0byB0
+aGUKICAgICAgU2FuIEFudG9uaW8gUml2ZXIuICBUaGVzZSB0d28gY3VsdHVy
+YWwgYW5kIG5hdHVyYWwgYXNzZXRzIHNob3VsZCB0byBiZQogICAgICBhY2Nl
+c3NpYmxlIHRvIHRoZSBjb21tdW5pdHkgb2YgdGhlIEJyb29rcyBBcmVhIFJl
+Z2lvbmFsIENlbnRlci4gIFNhbGFkbwogICAgICBDcmVlayBydW5zIHBhcmFs
+bGVsIHRvIHRoZSBTYW4gQW50b25pbyBSaXZlciBvbiB0aGUgZWFzdCBzaWRl
+IG9mIHRoZQogICAgICBCcm9va3MgQXJlYSBhbmQgc2hvdWxkIGJlIHJlY29n
+bml6ZWQgYXMgYSBuYXR1cmFsIGFzc2V0IHRoYXQgc2hvdWxkIGJlCiAgICAg
+IGNvbm5lY3RlZCBhbmQgdXNlZC4gQWRkaXRpb25hbGx5LCB0aGlzIGFyZWEg
+aXMgY2hhcmFjdGVyaXplZCBieSBiZWF1dGlmdWwKICAgICAgdG9wb2dyYXBo
+eSBhbmQgcmlwYXJpYW4gYXJlYXMsIHdoaWNoIHNob3VsZCBiZSB1c2VkIG5v
+dCBvbmx5IGZvcgogICAgICByZWNyZWF0aW9uLCBidXQgYXMgdGhlIHNpdGUg
+b2YgcGFyay1hbmQgdHJhaWwtb3JpZW50ZWQgZGV2ZWxvcG1lbnQgYXMKICAg
+ICAgZW52aXNpb25lZCBpbiB0aGUgPGEgaHJlZj0naHR0cHM6Ly9zYWNvbXBw
+bGFuLmNvbS9wbGFjZS10eXBlcy8nCiAgICAgIHRhcmdldD0nX2JsYW5rJz5T
+QSBUb21vcnJvdyBDb21wcmVoZW5zaXZlIFBsYW48L2E+LiBMYW5kIHVzZXMg
+b24gYWRqYWNlbnQKICAgICAgcHJvcGVydGllcyBzaG91bGQgY2FwdHVyZSB0
+aGUgcG90ZW50aWFsIHByb3ZpZGVkIGJ5IHRoZXNlIG5hdHVyYWwgYXNzZXRz
+LAogICAgICB3aGlsZSBtaW5pbWl6aW5nIHRoZSBpbXBhY3QgdGhhdCBkZXZl
+bG9wbWVudCBjYW4gaGF2ZSBvbiBlbnZpcm9ubWVudGFsbHkKICAgICAgc2Vu
+c2l0aXZlIGFyZWFzLgogICAgaW1hZ2U6IG51bGwKICAgIGltYWdlLTI6IG51
+bGwKICAtIHRpdGxlOiA+LQogICAgICA8aDQgY2xhc3M9J25vLW1hcmdpbic+
+UmVjb21tZW5kYXRpb24gIzQ6PC9oND4gUmVzcGVjdCBhbmQgcmVzcG9uZCB0
+bwogICAgICB0b3BvZ3JhcGh5IGFuZCBnZW9ncmFwaGljIGNvbnN0cmFpbnRz
+IHdoZW4gcGxhbm5pbmcgbmV3IGRldmVsb3BtZW50IGFuZAogICAgICBwdWJs
+aWMgaW1wcm92ZW1lbnRzLgogICAgY29udGVudDogPi0KICAgICAgVGhlIEJy
+b29rcyBBcmVhIGluY2x1ZGVzIHNpZ25pZmljYW50IHRvcG9ncmFwaHkgYW5k
+IG90aGVyIGdlb2dyYXBoaWMKICAgICAgZmVhdHVyZXMgdGhhdCBoYXZlIGhp
+c3RvcmljYWxseSBub3QgYmVlbiBhZGVxdWF0ZWx5IGFkZHJlc3NlZCBieQog
+ICAgICBkZXZlbG9wbWVudC4gV2hlbiBkZXZlbG9wbWVudCByZWd1bGF0aW9u
+cyBkbyBub3QgYWRlcXVhdGVseSBhY2NvdW50IGZvcgogICAgICBzdG9ybSB3
+YXRlciBydW5vZmYsIHRoZSByYWlucyB0aGF0IGFyZSBjaGFyYWN0ZXJpc3Rp
+YyBvZiB0aGUgU2FuIEFudG9uaW8KICAgICAgYXJlYSBjYW4gaGF2ZSBhIGRl
+dHJpbWVudGFsIGltcGFjdCBvbiBuZWlnaGJvcmluZyBwcm9wZXJ0aWVzLiAK
+ICAgICAgRnVydGhlcm1vcmUsIHRvcG9ncmFwaGljIGFuZCBnZW9ncmFwaGlj
+IGZlYXR1cmVzLCBzdWNoIGFzIGZsb29kcGxhaW5zIGFuZAogICAgICBzdGVl
+cCBzbG9wZXMsIGNhbiBtYWtlIGRldmVsb3BtZW50IG1vcmUgY29tcGxpY2F0
+ZWQgYW5kIGNvc3RseS4gTWVhc3VyZXMKICAgICAgdGhhdCByZXNwb25kIHRv
+IHRoZXNlIG5hdHVyYWwgY29uc3RyYWludHMgYW5kIGFjdGl2ZWx5IG1pbmlt
+aXplIHRoZQogICAgICBuZWdhdGl2ZSBpbXBhY3Qgb2YgZGV2ZWxvcG1lbnQg
+b24gbmF0dXJhbCBzeXN0ZW1zIGFyZSBuZWVkZWQgaW4gdGhlIEJyb29rcwog
+ICAgICBBcmVhIFJlZ2lvbmFsIENlbnRlci4KICAgIGltYWdlOiBudWxsCiAg
+ICBpbWFnZS0yOiBudWxsCiAgLSB0aXRsZTogPi0KICAgICAgPGg0IGNsYXNz
+PSduby1tYXJnaW4nPlJlY29tbWVuZGF0aW9uICM1OjwvaDQ+IEVuY291cmFn
+ZSBidXNpbmVzcwogICAgICByZXRlbnRpb24sIGNvbW1lcmNpYWwgcHJvcGVy
+dHkgc3RhYmlsaXR5IGFuZCBhZGFwdGl2ZSByZS11c2Ugd2hlcmUKICAgICAg
+YXBwcm9wcmlhdGUuCiAgICBjb250ZW50OiA+LQogICAgICBNYW55IG9mIHRo
+ZSBjb21tZXJjaWFsIGFyZWFzIGluIHRoZSBCcm9va3MgQXJlYSBSZWdpb25h
+bCBDZW50ZXIgYXJlCiAgICAgIGFnaW5nLiAgVGhpcyBtYWtlcyB0aGVtIHZ1
+bG5lcmFibGUgdG8gZGVjbGluZSBhbmQgZGlzcGxhY2VtZW50IGlmIGxhbmQg
+dXNlCiAgICAgIG1lYXN1cmVzIGFyZSBub3QgaW4gcGxhY2UgdG8gZW5jb3Vy
+YWdlIGJ1c2luZXNzIHJldGVudGlvbiwgcHJvcGVydHkKICAgICAgbWFpbnRl
+bmFuY2UsIHJlbm92YXRpb24gYW5kIGFkYXB0aXZlIHJlLXVzZS4gIE1haW50
+YWluaW5nIGV4aXN0aW5nCiAgICAgIGJ1c2luZXNzZXMgYW5kIGNvbW1lcmNp
+YWwgYXJlYXMgcHJvbW90ZXMgc3RhYmlsaXR5IGluIHByb3BlcnR5IHZhbHVl
+cyBmb3IKICAgICAgdGhlIGFyZWEgYW5kIGVuc3VyZXMgYWRlcXVhdGUgY29t
+bWVyY2lhbCBhbmQgZW1wbG95bWVudCBhY2Nlc3NpYmlsaXR5IGZvcgogICAg
+ICB0aGUgc3Vycm91bmRpbmcgbmVpZ2hib3Job29kcy4gRW5jb3VyYWdpbmcg
+YW5kIHN1cHBvcnRpbmcgaGlnaGVyLWludGVuc2l0eQogICAgICBkZXZlbG9w
+bWVudCBhbmQgYSBtaXggb2YgdXNlcyB0aHJvdWdoIE1peGVkLVVzZSBsYW5k
+IHVzZSBkZXNpZ25hdGlvbnMgY2FuCiAgICAgIHN1cHBvcnQgdGhlc2UgZ29h
+bHMuIE1lYXN1cmVzIHRoYXQgYWNjb21tb2RhdGUgcmV2aXRhbGl6YXRpb24g
+ZWZmb3J0cyBhbmQKICAgICAgYnVzaW5lc3Mgdml0YWxpdHkgc2hvdWxkIGJl
+IGVtcGxveWVkIGluIGNvbW1lcmNpYWwgYXJlYXMgd2hlcmUKICAgICAgcHJl
+c2VydmF0aW9uIG9mIGV4aXN0aW5nIGJ1c2luZXNzZXMgb2YgdmFsdWUgdG8g
+dGhlIGNvbW11bml0eS4KICAgIGltYWdlOiBzZGYKICAgIGltYWdlLTI6IG51
+bGwKCi0tLQoK
+
